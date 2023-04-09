4526984029986582b4b941b4ae347468df0aa866
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-04-09 20:48:36 +0700

    perbaiki typo

--- 04 HIgh Order Funtion/HOF.js
+++ 04 HIgh Order Funtion/HOF.js
@@ -1,5 +1,5 @@
 //! ===================== HIGH ORDER FUNCTION (HOF) ===========================
-//? merupakakan function yang memiliki parameter function
+//? merupakan function yang memiliki parameter function
 //? function yang menjadi parameter di sebut callback
 
 const angka = [-1, 2, 5, -2, 4, 8, -3, 10, 9, -6];
